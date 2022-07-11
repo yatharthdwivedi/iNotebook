3fbc344742c63c3c3a43e99df361e67a8e3805b9
yatharthdwivedi <yatharthdwivedi1@gmail.com>
2022-07-12 02:53:54 +0530

    d

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
   return (
     <div className="App">
       <NoteState>
-        <BrowserRouter>
+        <BrowserRouter basename="/iNotebook">
           <Navbar />
           <Alert alert={alert} />
           <div className="container">
