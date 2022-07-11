4fdb24415c64f325199329414130e2333aacd9d1
yatharthdwivedi <yatharthdwivedi1@gmail.com>
2022-07-11 17:10:50 +0530

    Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ function Navbar() {
   }
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">
           iNotebook
